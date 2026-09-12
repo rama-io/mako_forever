5ca461a37588509970d8174971893e142d25b2b5
Pomboverso <github.5yeil@simplelogin.fr>
2026-09-12 06:00:57 -0700

    migration

--- app/src/main/java/com/rama/mako_forever/activities/Settings.java
+++ app/src/main/java/com/rama/mako_forever/activities/Settings.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.widget.Button;
 import com.rama.mako_forever.R;
 import com.rama.mako_forever.managers.FontManager;
-import android.content.Intent;
 
 public class Settings extends Activity {
 
