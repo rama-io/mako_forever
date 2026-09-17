d5f2ce11182ea83915072cd20131d3907d52dc78
Pomboverso <github.5yeil@simplelogin.fr>
2026-09-17 14:34:15 -0700

    adding new themes

--- app/src/main/java/com/rama/mako_zero/objects/Themes.java
+++ app/src/main/java/com/rama/mako_zero/objects/Themes.java
@@ -43,140 +43,19 @@ public final class Themes {
     private Themes() {
     }
 
-    public static final Palette CATPPUCCIN_MOCHA_BLUE = new Palette(PrefTheme.CATPPUCCIN_MOCHA_BLUE, "Blue", 0xFFCDD6F4, // text
-            0xFF1E1E2E, // base
-            0xFF45475A, // surface_0
-            0xFF585B70, // surface_1
-            0xFF587D52, // subtle
-            0xFF585B70, // disabled
-            0xFF585B70, // border
-            0xFF89b4fa, // accent
-            0xFFA6E3A1, // success
-            0xFFFFD700, // warning
-            0xFFF38BA8, // error
-            0xFF89DCEB, // info
-            0xFF89DCEB // link
-    );
+    private static Palette mocha(String id, String label, int accent) {
+        return new Palette(id, label, 0xFFCDD6F4, 0xFF1E1E2E, 0xFF45475A, 0xFF585B70, 0xFF587D52, 0xFF585B70, 0xFF585B70, accent, 0xFFA6E3A1, 0xFFFFD700, 0xFFF38BA8, 0xFF89DCEB, 0xFF89DCEB);
+    }
 
-    public static final Palette CATPPUCCIN_MOCHA_MAUVE = new Palette(PrefTheme.CATPPUCCIN_MOCHA_MAUVE, "Mauve", 0xFFCDD6F4, // text
-            0xFF1E1E2E, // base
-            0xFF45475A, // surface_0
-            0xFF585B70, // surface_1
-            0xFF587D52, // subtle
-            0xFF585B70, // disabled
-            0xFF585B70, // border
-            0xFFcba6f7, // accent
-            0xFFA6E3A1, // success
-            0xFFFFD700, // warning
-            0xFFF38BA8, // error
-            0xFF89DCEB, // info
-            0xFF89DCEB // link
-    );
-
-    public static final Palette CATPPUCCIN_MOCHA_PEACH = new Palette(PrefTheme.CATPPUCCIN_MOCHA_PEACH, "Peach", 0xFFCDD6F4, // text
-            0xFF1E1E2E, // base
-            0xFF45475A, // surface_0
-            0xFF585B70, // surface_1
-            0xFF587D52, // subtle
-            0xFF585B70, // disabled
-            0xFF585B70, // border
-            0xFFfab387, // accent
-            0xFFA6E3A1, // success
-            0xFFFFD700, // warning
-            0xFFF38BA8, // error
-            0xFF89DCEB, // info
-            0xFF89DCEB // link
-    );
-
-    public static final Palette CATPPUCCIN_MOCHA_RED = new Palette(PrefTheme.CATPPUCCIN_MOCHA_RED, "Red", 0xFFCDD6F4, // text
-            0xFF1E1E2E, // base
-            0xFF45475A, // surface_0
-            0xFF585B70, // surface_1
-            0xFF587D52, // subtle
-            0xFF585B70, // disabled
-            0xFF585B70, // border
-            0xFFf38ba8, // accent
-            0xFFA6E3A1, // success
-            0xFFFFD700, // warning
-            0xFFF38BA8, // error
-            0xFF89DCEB, // info
-            0xFF89DCEB // link
-    );
-
-    public static final Palette CATPPUCCIN_MOCHA_YELLOW = new Palette(PrefTheme.CATPPUCCIN_MOCHA_YELLOW, "Yellow", 0xFFCDD6F4, // text
-            0xFF1E1E2E, // base
-            0xFF45475A, // surface_0
-            0xFF585B70, // surface_1
-            0xFF587D52, // subtle
-            0xFF585B70, // disabled
-            0xFF585B70, // border
-            0xFFf9e2af, // accent
-            0xFFA6E3A1, // success
-            0xFFFFD700, // warning
-            0xFFF38BA8, // error
-            0xFF89DCEB, // info
-            0xFF89DCEB // link
-    );
-
-    public static final Palette CATPPUCCIN_MOCHA_GREEN = new Palette(PrefTheme.CATPPUCCIN_MOCHA_GREEN, "Green", 0xFFCDD6F4, // text
-            0xFF1E1E2E, // base
-            0xFF45475A, // surface_0
-            0xFF585B70, // surface_1
-            0xFF587D52, // subtle
-            0xFF585B70, // disabled
-            0xFF585B70, // border
-            0xFFa6e3a1, // accent
-            0xFFA6E3A1, // success
-            0xFFFFD700, // warning
-            0xFFF38BA8, // error
-            0xFF89DCEB, // info
-            0xFF89DCEB // link
-    );
-
-    public static final Palette CATPPUCCIN_LATTE = new Palette(PrefTheme.CATPPUCCIN_LATTE, "Latte", 0xFF4C4F69, // text
-            0xFFEFF1F5, // base
-            0xFFBCC0CC, // surface_0
-            0xFFBCC0CD, // surface_1
-            0xFF8CCB9A, // subtle
-            0xFFBCC0CD, // disabled
-            0xFFBCC0CD, // border
-            0xFF1E66F5, // accent
-            0xFF40A02B, // success
-            0xFFFE640B, // warning
-            0xFFD20F39, // error
-            0xFF04A5E5, // info
-            0xFF04A5E5 // link
-    );
-
-    public static final Palette MONO_DARK = new Palette(PrefTheme.MONO_DARK, "Mono Dark", 0xFFFFFFF9, // text
-            0xFF040100, // base
-            0xFF040101, // surface_0
-            0xFF040200, // surface_1
-            0xFFF9FFFF, // subtle
-            0xFF040200, // disabled
-            0xFF040200, // border
-            0xFFFFF9F9, // accent
-            0xFFFBFFFF, // success
-            0xFFFBFEFF, // warning
-            0xFFFBFFFB, // error
-            0xFFFFFAFA, // info
-            0xFFFFFAFA // link
-    );
-
-    public static final Palette MONO_LIGHT = new Palette(PrefTheme.MONO_LIGHT, "Mono Light", 0xFF000040, // text
-            0xFFFFFFFF, // base
-            0xFFFFFEFF, // surface_0
-            0xFFFEFFFF, // surface_1
-            0xFF040000, // subtle
-            0xFFFEFFFF, // disabled
-            0xFFFEFFFF, // border
-            0xFF020000, // accent
-            0xFF000001, // success
-            0xFF010000, // warning
-            0xFF020002, // error
-            0xFF000303, // info
-            0xFF000303 // link
-    );
+    public static final Palette CATPPUCCIN_MOCHA_MAUVE = mocha(PrefTheme.CATPPUCCIN_MOCHA_MAUVE, "Mauve", 0xFFCBA6F7);
+    public static final Palette CATPPUCCIN_MOCHA_BLUE = mocha(PrefTheme.CATPPUCCIN_MOCHA_BLUE, "Blue", 0xFF89B4FA);
+    public static final Palette CATPPUCCIN_MOCHA_YELLOW = mocha(PrefTheme.CATPPUCCIN_MOCHA_YELLOW, "Yellow", 0xFFF9E2AF);
+    public static final Palette CATPPUCCIN_MOCHA_PEACH = mocha(PrefTheme.CATPPUCCIN_MOCHA_PEACH, "Peach", 0xFFFAB387);
+    public static final Palette CATPPUCCIN_MOCHA_GREEN = mocha(PrefTheme.CATPPUCCIN_MOCHA_GREEN, "Green", 0xFFA6E3A1);
+    public static final Palette CATPPUCCIN_MOCHA_RED = mocha(PrefTheme.CATPPUCCIN_MOCHA_RED, "Red", 0xFFF38BA8);
+    public static final Palette CATPPUCCIN_LATTE = new Palette(PrefTheme.CATPPUCCIN_LATTE, "Latte", 0xFF4C4F69, 0xFFEFF1F5, 0xFFBCC0CC, 0xFFBCC0CD, 0xFF8CCB9A, 0xFFBCC0CD, 0xFFBCC0CD, 0xFF1E66F5, 0xFF40A02B, 0xFFFE640B, 0xFFD20F39, 0xFF04A5E5, 0xFF04A5E5);
+    public static final Palette MONO_DARK = new Palette(PrefTheme.MONO_DARK, "Mono Dark", 0xFFFFFFF9, 0xFF040100, 0xFF040101, 0xFF040200, 0xFFF9FFFF, 0xFF040200, 0xFF040200, 0xFFFFF9F9, 0xFFFBFFFF, 0xFFFBFEFF, 0xFFFBFFFB, 0xFFFFFAFA, 0xFFFFFAFA);
+    public static final Palette MONO_LIGHT = new Palette(PrefTheme.MONO_LIGHT, "Mono Light", 0xFF000040, 0xFFFFFFFF, 0xFFFFFEFF, 0xFFFEFFFF, 0xFF040000, 0xFFFEFFFF, 0xFFFEFFFF, 0xFF020000, 0xFF000001, 0xFF010000, 0xFF020002, 0xFF000303, 0xFF000303);
 
     public static List<Palette> all() {
         List<Palette> list = new ArrayList<Palette>();
@@ -195,7 +74,9 @@ public final class Themes {
     public static Palette byId(String id) {
         List<Palette> list = all();
         for (int i = 0; i < list.size(); i++) {
-            if (list.get(i).id.equals(id)) return list.get(i);
+            if (list.get(i).id.equals(id)) {
+                return list.get(i);
+            }
         }
         return CATPPUCCIN_MOCHA_YELLOW;
     }
